9490fbe6f4f08bccad65c5a7cfa77318ffe020c9
Seth Lauer <seth.j.lauer@gmail.com>
2021-07-08 21:30:07 -0700

    show pages

---
+++ polesexual-productions/src/components/Shows/PoleSexual.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ShowPage from './ShowPage';
+
+const PoleSexual = () => {
+    return <ShowPage name="PoleSexual" img="" nextDate="August 1st" />
+}
+
+export default PoleSexual;
---
+++ polesexual-productions/src/components/Shows/Power.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ShowPage from './ShowPage';
+
+const Power = () => {
+    return <ShowPage name="Power" img="" nextDate="August 22nd" />
+}
+
+export default Power;
