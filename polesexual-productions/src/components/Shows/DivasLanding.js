c3cc6261c5e907ceda153a6d575717fa5284687c
mikey Strider <dromen2001@gmail.com>
2021-07-10 12:28:42 -0700

    Changed date for and name for Divas Landing

--- polesexual-productions/src/components/Shows/DivasLanding.js
+++ polesexual-productions/src/components/Shows/DivasLanding.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ShowPage from './ShowPage';
 
 const DivasLanding = () => {
-    return <ShowPage name="CockTailGate" img="" nextDate="July 18th" />
+    return <ShowPage name="Divas Landing" img="" nextDate="July 31st" />
 }
 
 export default DivasLanding;
